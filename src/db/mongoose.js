ec14fd522bf264c8dc01e1de826cae2d2993de0c
kwamkert <tomriddle477@gmail.com>
2020-04-08 22:12:44 +0000

    connectionurl added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1/task-manager-api',{
+const connectionUrl = 'mongodb://127.0.0.1/task-manager-api';
+
+
+mongoose.connect(connectionUrl,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-})
+}, (error, client) =>{{
+
+    if(error){
+        return console.log('unable to connect to database')
+    }
+
+    console.log("Connected correctly")
+}})
 

518b98d31fb779e957538a3fea8558f39bee742a
kwamkert <tomriddle477@gmail.com>
2020-04-16 19:54:30 +0000

    Auth updated

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,18 +28,45 @@ router.post('/tasks',auth,async (req,res)=>{
      } 
  })
  
- router.get('/tasks/:id',async (req,res)=>{
+
+//  router.get('/tasks/:id', auth, async (req, res) => {
+//     const _id = req.params.id
+ 
+
+//     try {
+//        const task =  await Task.findOne({ _id, owner: req.user._id })
+
+//        console.log(req.user._id)
+
+//         // if (!task) {
+//         //     return res.status(403).send()
+//         // }
+
+//        // console.log(task)
+//         //res.send(task)
+//         res.status(200).send()
+//     } catch (e) {
+//         console.log(e)
+//         res.status(500).send()
+//     }
+// })
+
+
+ router.get('/tasks/:id',auth, async (req,res)=>{
      const _id = req.params.id
+
      try {
-         const task = await Task.findById(_id)
+         const task = await  Task.findOne({_id, owner:req.user._id})
+
+
          if(!task){
-             return res.status(404).send("Task does not exist")
-         }
-         res.send(task)
+             return res.status(400).send("Task does not exist")
+         }      
+        return res.status(200).send(task)
      }catch(e){
+       
          res.status(500).send(e)
      }
-   
  })
  
  

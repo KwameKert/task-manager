9eea54f9db20c4ec69a0e90fbca96b35c79c9d1a
kwamkert <tomriddle477@gmail.com>
2020-04-16 20:14:59 +0000

    delete task completed

--- src/routers/task.js
+++ src/routers/task.js
@@ -72,11 +72,11 @@ router.post('/tasks',auth,async (req,res)=>{
  })
  
  
- router.delete('/tasks/:id', async (req,res)=>{
+ router.delete('/tasks/:id', auth, async (req,res)=>{
      const _id = req.params.id
  
      try{
-         const task =await  Task.findByIdAndDelete(_id)
+         const task = await  Task.findOneAndDelete({_id, owner: req.user._id})
          if(!task){
              res.status(400).send({"error":"Task not found"})
          }
